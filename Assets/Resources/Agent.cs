d1ac92172a957cf9cc9dfbf9c06d28d310251932
Mike Gao <contact@mikegao.net>
2020-11-22 17:14:25 -0500

    Hopefully the last bugfix

--- Assets/Resources/Agent.cs
+++ Assets/Resources/Agent.cs
@@ -143,7 +143,8 @@ public class Agent : MonoBehaviour
         var delta = other.transform.position - transform.position;
         var offset = radius * 2 - delta.magnitude;
         delta.Normalize();
-        transform.position -= 0.75f * offset * delta;
+        transform.position -= 2.00f * offset * delta;
+        other.transform.position += 2.00f * offset * delta;
         Invoke("RetryDestination", Random.Range(0.1f, 0.5f));
     }
 
